10b9be028b06ebb105294dec71c618d89f370eb3
jimmywu-commits <jimmy.wu@shopee.com>
2026-06-18 11:38:09 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,20 +3,28 @@
    ★ 排版清單，BN編輯器從這裡讀取版位
 ══════════════════════════════════════ */
 var BN_LAYOUTS = [
-  "SCBN_APP.html",
-  "IG方logo排版.html",
   "IG橫logo排版.html",
-  "HBN_方式LOGO.html",
+  "IG方logo排版.html",
+
   "HBN_橫式LOGO.html",
+  "HBN_方式LOGO.html",
+
   "ddcard方logo.html",
   "ddcard橫logo.html",
+
   "Coin_pageBN_APP方LOGO.html",
   "Coin_pageBN_APP橫LOGO.html",
+
   "FB_POST_方LOGO.html",
   "FB_POST_橫LOGO.html",
+
   "LPBN_APP方LOGO.html",
+  "LPBN_PC方LOGO.html",
   "LPBN_APP橫LOGO.html",
   "LPBN_PC橫LOGO.html",
+
+  "SCBN_APP.html",
+
   "Search_Image1logo.html",
   "Search_Image2logo.html",
   "Search_Image3logo.html"
